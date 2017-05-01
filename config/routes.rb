eebe05e62f5146d61e91fb0a962044b8bef428ab
Alex Gould <alexgould17@gmail.com>
2017-04-30 21:50:46 -0700

    Added sign up & sign in/out links + confirmed they work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Home controller (top-level) paths. index is our root.
   root 'home#index'
   get 'about', to: "home#about"
   get 'faq', to: "home#faq"
+
+  # User signup/login/etc. paths via devise
+  devise_for :users
 end
