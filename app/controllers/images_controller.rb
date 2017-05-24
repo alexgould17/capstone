e21647287e3934f393bfb96dc3511ca51d32b18f
Alex Gould <alexgould17@gmail.com>
2017-05-24 00:14:24 -0700

    Finished all the CRUD routes/views/controller actions for images. Updated the before_save to not strip commas from tags >_>. Tested & confirmed to work.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,8 @@
 class ImagesController < ApplicationController
-  # index corresponds to a user's collection
+  # Must be logged in to create, edit or delete.
+  before_action :authenticate_user!, except: [:index, :show]
+
+  # index corresponds to a user's collection.
   def index
     @images = Image.where(user_id: params[:id])
     @user = User.find(params[:id])
@@ -13,14 +16,15 @@ class ImagesController < ApplicationController
     @image = Image.new(image_params)
     @image.user = current_user
     if @image.save
-      flash[:notice] << "Added image \"#{@image.title}\" to your collection!"
+      flash[:notice] = "Added image \"#{@image.title}\" to your collection!"
       redirect_to @image
     else
       flash[:alert] = "Couldn't save image \"#{@image.title}\". Please try again."
+      render :new
     end
   end
 
-  # show corresponds to a single image view
+  # show corresponds to a single image view.
   def show
     @image = Image.find(params[:id])
     @tags = @image.tags.split(',')
@@ -31,11 +35,26 @@ class ImagesController < ApplicationController
   end
 
   def update
-
+    @image = Image.find(params[:id])
+    @image.assign_attributes(image_params)
+    if @image.save
+      flash[:notice] = "Image \"#{@image.title}\" updated successfully!"
+      redirect_to @image
+    else
+      flash[:alert] = "Couldn't update image \"#{@image.title}\". Please try again."
+      render :edit
+    end
   end
 
   def destroy
-
+    @image = Image.find(params[:id])
+    if @image.destroy
+      flash[:notice] = "Image \"#{@image.title}\" deleted."
+      redirect_to current_user
+    else
+      flash[:alert] = "Couldn't delete image \"#{@image.title}\". Please try again."
+      render :show
+    end
   end
 
   private
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,28 +2,31 @@ class Image < ApplicationRecord
   belongs_to :user
 
   before_save do
-    flash[:notice] = []
+    self.latitude = self.latitude.to_f unless self.latitude.class == 'Float'
+    self.longitude = self.longitude.to_f unless self.longitude.class == 'Float'
+
     # Make sure latitude is between -90 & 90 degrees
     if self.latitude < -90
       self.latitude = -90
-      flash[:notice] << "Latitude can't be less than -90!"
     elsif self.latitude > 90
       self.latitude = 90
-      flash[:notice] << "Latitude can't be greater than 90!"
     end
 
     # Make sure longitude is between -180 & 180 degrees
     if self.longitude < -180
       self.longitude = -180
-      flash[:notice] << "Longitude can't be less than -180!"
     elsif self.longitude > 180
       self.longitude = 180
-      flash[:notice] << "Longitude can't be greater than 180!"
     end
 
-    # Set pictype to :picture if it is undefined, out of range, wrong type, etc.
-    if self.pictype != :picture && self.pictype != :artwork && self.pictype != :other
-      self.pictype = :picture
+    if self.pictype == :picture
+      self.pictype = 0
+    elsif self.pictype == :artwork
+      self.pictype = 1
+    elsif self.pictype == :other
+      self.pictype = 2
+    else
+      self.pictype = 0
     end
 
     # Make sure width & height are integers
@@ -33,8 +36,7 @@ class Image < ApplicationRecord
     self.aspect_ratio = width.to_f / height.to_f
 
     # Filter unwanted characters from the tags
-    badchars = self.tags.gsub!(/[^0-9a-zA-Z\-_\+=~\.\?\$;:'"@#\$%\^\&\*\(\)\[\]\{\}\\\/<>]/,'')
-    flash[:notice] << "Unsupported characters detected in tags!" if badchars
+    self.tags.gsub!(/[^0-9a-zA-Z\-_\+=~\.\?\$;:'"@#\$%\^\&\*\(\)\[\]\{\}\\\/<>,]/,'')
   end
 
   enum pictype: [:picture, :artwork, :other]
