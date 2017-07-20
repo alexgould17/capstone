fd05ee46e27a5962481648d9e4f8485943fda22e
Alex Gould <alexgould17@gmail.com>
2017-07-19 22:15:21 -0700

    Updated email server in production environment to reflect Heroku app name change.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Setup actionmailer for devise using herok as our host
-  config.action_mailer.default_url_options = { host: 'enigmatic-sierra-35213.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'ag17-capstone.herokuapp.com' }
 end
